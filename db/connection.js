a4d4f389a00bd1f3c7197a07e8dffc33caf49d69
lsmith32 <lmksmith97@gmail.com>
2022-04-03 16:06:26 -0500

    functions added to department, employee, and role

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,11 @@
 const mysql = require('mysql2');
 
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Lasm#0289',
+    database: 'managment',
+    port: 3303
+});
+
 module.exports = db;
